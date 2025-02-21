bfe791546fc4b647afbc4395948a95a08f3845f4
Pavan9567 <pavank9567@gmail.com>
2025-02-21 22:14:11 +0530

    Article APP

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: process.env.ALLOWED_ORIGIN || "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
