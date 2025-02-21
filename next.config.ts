a93c242f7e097f03a8eebde42128d0ad01b74f43
Pavan9567 <pavank9567@gmail.com>
2025-02-21 22:19:13 +0530

    Article APP

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "api/:path*",
+        source: "/api/",
         headers: [
           { key: "Access-Control-Allow-Origin", value: process.env.ALLOWED_ORIGIN || "*" },
           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE" },
